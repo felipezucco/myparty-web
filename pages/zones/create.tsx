197812b93d3a8717ef7952ea819ab49f3e04ab51
Felipe <felipecostazucco@gmail.com>
2022-02-08 00:25:11 -0300

    melhora tela de criação de ambientes

--- pages/zones/create.tsx
+++ pages/zones/create.tsx
@@ -1,39 +1,85 @@
 import { InputText } from "primereact/inputtext";
-import { useState } from "react";
+import { FC, useState } from "react";
 import { Zone } from "../../models/Zone.type";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { GetServerSideProps } from "next";
 import { parseCookies } from "nookies";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import DialogTitle from "@mui/material/DialogTitle";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 
-const CreateZoneComponent = () => {
+interface CreateZoneInterface {
+  show: (b: boolean) => void
+}
+
+const CreateZoneComponent: FC<CreateZoneInterface> = ({ show }) => {
 
   const { register, handleSubmit } = useForm<Zone>();
-  const [zone, setZone] = useState<Zone>();
+  const [success, setSuccess] = useState(false);
 
   const handleSubmitForm = (data: any): SubmitHandler<Zone> => {
-    console.log(data)
+    console.log(data);
+    setSuccess(true);
+    setTimeout(() => setSuccess(false), 3000);
     return data;
   }
 
   return (
-    <form onSubmit={handleSubmit(handleSubmitForm)}>
-      <div className="p-grid">
-        <div className="p-col">
-          <label htmlFor="zoneName" className="p-d-block">Nome do Ambiente</label>
-          <InputText id="zoneName" {...register('name')}
-            className="p-d-block" onChange={(e) => setZone({ ...zone, name: e.target.value })} />
-        </div>
-        <div className="p-col">
-          <label htmlFor="zoneMetric" className="p-d-block">Metragem</label>
-          <InputText id="zoneMetric" {...register('metric')} value={zone?.metric?.toString()} />
-        </div>
-        <div className="p-col">
-          <label htmlFor="zoneColor" className="p-d-block">Cor</label>
-          <input type={'color'} {...register('color')} />
-        </div>
-      </div>
-      <button>Teste</button>
-    </form>
+    <>
+      <Snackbar
+        open={success}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+        <Alert
+          severity="success"
+          action={
+            <IconButton color="inherit" size="small" onClick={() => setSuccess(false)}>
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }>
+          Novo ambiente criado com sucesso!
+        </Alert>
+      </Snackbar>
+      <form onSubmit={handleSubmit(handleSubmitForm)}>
+        <DialogTitle>
+          {`Novo Ambiente`}
+        </DialogTitle>
+        <DialogContent>
+          <Grid container>
+            <Grid item xs={12}>
+              <TextField
+                label="Nome do Ambiente"
+                id="outlined-size-small"
+                {...register('name')}
+                fullWidth
+                margin="normal"
+                size="small" />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Metragem"
+                id="outlined-size-small"
+                {...register('metric')}
+                margin="normal"
+                size="small" />
+            </Grid>
+            <Grid item xs={6}>
+              <input type={'color'} {...register('color')} />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button type="button" onClick={() => show(false)}>Cancelar</Button>
+          <Button type="submit" /*onClick={() => show(false)}*/ autoFocus>Criar Novo Ambiente</Button>
+        </DialogActions>
+      </form>
+    </>
   );
 }
 
--- pages/zones/index.tsx
+++ pages/zones/index.tsx
@@ -1,12 +1,10 @@
-import Link from "next/link";
-import { Button } from "primereact/button";
-import { FunctionComponent, ReactElement, useState } from "react";
-import { Dialog } from 'primereact/dialog';
-import CreateLocal from "../local";
+import { ReactElement, useState } from "react";
 import CreateZoneComponent from "./create";
 import LayoutComponent from "../../comps/Layout/Layout";
 import { GetServerSideProps } from "next";
 import { parseCookies } from "nookies";
+import Dialog from '@mui/material/Dialog';
+import Button from "@mui/material/Button";
 
 const ZonesComponent = () => {
 
@@ -14,12 +12,9 @@ const ZonesComponent = () => {
 
   return (
     <>
-      <Button label="Novo Ambiente" icon="pi pi-add" iconPos="right" onClick={() => setDisplay(true)} />
-      {/* <Link href={'/zones/create'}>
-        <Button label="Novo Ambiente" icon="pi pi-add" iconPos="right" />
-      </Link> */}
-      <Dialog header="Novo Ambiente" visible={display} style={{ width: '50vw' }} onHide={() => setDisplay(false)}>
-        <CreateZoneComponent />
+      <Button onClick={() => setDisplay(true)} >Novo Ambiente</Button>
+      <Dialog open={display}>
+        <CreateZoneComponent show={setDisplay} />
       </Dialog>
     </>
   );
